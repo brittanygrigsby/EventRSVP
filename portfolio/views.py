db3fbd33615125c210d75013ed1fc1523508e157
Brittany Grigsby <brittanygrigsby@Mac.lan>
2025-11-19 01:03:50 -0800

    Add new files

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .forms import ContactForm
+from django.core.mail import send_mail
 
 # Create your views here.
 def about_me_view(request):
@@ -8,4 +10,39 @@ def experience_view(request):
     return render(request, 'portfolio/experience.html')
 
 def contact_view(request):
-    return render(request, 'portfolio/contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+    
+        #collect all data from the contact fields
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            #Build the full email
+
+            message_body = (
+                f'You have a new email from your portfolio\n'
+                f'Name: {name}\n'
+                f'Email: {email}\n'
+                f'Message: {message}\n'
+            )
+        
+            try:
+                send_mail(
+                    "Email From portfolio", # Subhect
+                    message_body, # Message body ---> the user typed
+                    email, # Form Email user Email
+                    ['brittanysugrigsby@gmail.com']
+
+                )
+                form = ContactForm()
+                return render(request,'portfolio/contact.html',{'form':form})
+            except Exception as e:
+                print(f'Error sending email:{e}')
+                return render(request,'portfolio/contact.html',{'form':form})
+                
+        else:
+            print("Invalid data")
+    else:
+        form = ContactForm()
+        return render(request, 'portfolio/contact.html',{'form':form})
